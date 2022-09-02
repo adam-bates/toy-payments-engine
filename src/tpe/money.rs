8170acacba7f06b9922ad28fae052513b8e79431
adam-bates <adam@adambates.ca>
2022-09-02 14:39:23 -0400

    Added unit tests for money.

--- src/tpe/money.rs
+++ src/tpe/money.rs
@@ -76,7 +76,7 @@ impl Money {
 
 impl fmt::Display for Money {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let string = format!("{:0<5}", self.0);
+        let string = format!("{:0>5}", self.0);
 
         let pivot = string.len() - 4;
 
@@ -84,6 +84,107 @@ impl fmt::Display for Money {
         let cents = &string[pivot..];
 
         return write!(f, "{dollars}.{cents}");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse() {
+        assert_eq!(Money::parse("0".to_string()).unwrap(), Money(0));
+        assert_eq!(Money::parse("0.".to_string()).unwrap(), Money(0));
+        assert_eq!(Money::parse("0.0".to_string()).unwrap(), Money(0));
+        assert_eq!(Money::parse("1.0".to_string()).unwrap(), Money(10000));
+        assert_eq!(Money::parse("123456".to_string()).unwrap(), Money(1234560000));
+        assert_eq!(Money::parse("123456.001".to_string()).unwrap(), Money(1234560010));
+        assert_eq!(Money::parse("0.1234".to_string()).unwrap(), Money(1234));
+        assert_eq!(Money::parse("1234.5678".to_string()).unwrap(), Money(12345678));
+        assert_eq!(Money::parse("922337203685477.5807".to_string()).unwrap(), Money(std::i64::MAX));
+    }
+
+    #[test]
+    fn parse_only_reads_4_digits() {
+        assert_eq!(Money::parse("0.123456".to_string()).unwrap(), Money(1234));
+        assert_eq!(Money::parse("9000.00001".to_string()).unwrap(), Money(90000000));
+    }
+
+    #[test]
+    fn fail_to_parse_invalid_str() {
+        assert_eq!(Money::parse("".to_string()).is_ok(), false);
+    }
+
+    #[test]
+    fn fail_to_parse_too_big() {
+        assert_eq!(Money::parse(std::u128::MAX.to_string()).is_ok(), false);
+    }
+
+    #[test]
+    fn fail_to_parse_too_small() {
+        assert_eq!(Money::parse(std::i128::MIN.to_string()).is_ok(), false);
+    }
+
+    #[test]
+    fn serialize() {
+        assert_eq!(&Money(0).to_string(), "0.0000");
+        assert_eq!(&Money(1).to_string(), "0.0001");
+        assert_eq!(&Money(10010).to_string(), "1.0010");
+        assert_eq!(&Money(12345678).to_string(), "1234.5678");
+        assert_eq!(&Money(543210000).to_string(), "54321.0000");
+        assert_eq!(&Money::MAX.to_string(), "922337203685477.5807");
+    }
+
+    #[test]
+    fn add() {
+        let mut a = Money(123);
+        let b = Money(456);
+
+        a.add(&b).unwrap();
+
+        assert_eq!(a, Money(123 + 456));
+    }
+
+    #[test]
+    fn fail_to_add_overflow() {
+        let mut a = Money::MAX;
+        let b = Money(1);
+
+        assert_eq!(a.add(&b).is_ok(), false);
+    }
+
+    #[test]
+    fn fail_to_add_underflow() {
+        let mut a = Money::MIN;
+        let b = Money(-1);
+
+        assert_eq!(a.add(&b).is_ok(), false);
+    }
+
+    #[test]
+    fn sub() {
+        let mut a = Money(456);
+        let b = Money(123);
+
+        a.sub(&b).unwrap();
+
+        assert_eq!(a, Money(456 - 123));
+    }
+
+    #[test]
+    fn fail_to_sub_overflow() {
+        let mut a = Money::MAX;
+        let b = Money(-1);
+
+        assert_eq!(a.sub(&b).is_ok(), false);
+    }
+
+    #[test]
+    fn fail_to_sub_underflow() {
+        let mut a = Money::MIN;
+        let b = Money(1);
+
+        assert_eq!(a.sub(&b).is_ok(), false);
     }
 }
 
