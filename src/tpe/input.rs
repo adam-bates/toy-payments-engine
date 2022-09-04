8ca2326db234de32030ad41ffb571a4e1f043803
adam-bates <adam@adambates.ca>
2022-09-03 21:11:23 -0400

    Prevented negative amounts.

--- src/tpe/input.rs
+++ src/tpe/input.rs
@@ -9,7 +9,7 @@ use serde::Deserialize;
 use thiserror::Error;
 
 /// Represents an input event that a string would deserialize into
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct InputEvent {
     #[serde(rename = "type")]
     pub typ: InputEventType,
@@ -19,7 +19,7 @@ pub struct InputEvent {
     pub amount: Option<String>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 #[serde(rename_all = "lowercase")]
 pub enum InputEventType {
     Deposit,
@@ -31,19 +31,29 @@ pub enum InputEventType {
 
 #[derive(Error, Debug)]
 pub enum InputParseError {
-    #[error("No deposit amount")]
-    NoDepositAmount,
+    #[error("Error parsing input event: amount value missing from deposit: {0:?}")]
+    NoDepositAmount(InputEvent),
 
-    #[error("No deposit amount")]
-    NoWithdrawalAmount,
+    #[error("Error parsing input event: amount value missing from withdrawal: {0:?}")]
+    NoWithdrawalAmount(InputEvent),
+
+    #[error("Error parsing input event: negative amount values not supported: {0:?}")]
+    NegativeAmount(InputEvent),
 }
 
 impl InputEvent {
     pub fn parse_transaction(self) -> Result<Transaction> {
         let tx = match self.typ {
             InputEventType::Deposit => {
-                let amount = self.amount.ok_or(InputParseError::NoDepositAmount)?;
+                let amount = self
+                    .clone()
+                    .amount
+                    .ok_or_else(|| InputParseError::NoDepositAmount(self.clone()))?;
                 let amount = Money::parse(amount)?;
+
+                if amount.0 < 0 {
+                    Err(InputParseError::NegativeAmount(self.clone()))?;
+                }
 
                 Transaction {
                     id: TransactionId(self.tx),
@@ -53,8 +63,12 @@ impl InputEvent {
                 }
             }
             InputEventType::Withdrawal => {
-                let amount = self.amount.ok_or(InputParseError::NoDepositAmount)?;
+                let amount = self.amount.clone().ok_or_else(|| InputParseError::NoWithdrawalAmount(self.clone()))?;
                 let amount = Money::parse(amount)?;
+
+                if amount.0 < 0 {
+                    Err(InputParseError::NegativeAmount(self.clone()))?;
+                }
 
                 Transaction {
                     id: TransactionId(self.tx),
